cd4afa8bf49b004dc1a56c1eb417b9bc8dfb291f
sadra <sadrataghavi1383@gmail.com>
2022-08-30 14:16:56 +0430

    feat(navbar): title

--- app/layout/Navbar.tsx
+++ app/layout/Navbar.tsx
@@ -5,8 +5,8 @@ import './style/navbar.scss'
 const Navbar = () => {
     return (
         <div className='nav-container'>
-            <div className='nav-wrapper'>
-                <div className='nav-logo'></div>
+            <div className='nav-wrapper title_small'>
+                <div className='nav-logo'>LOGO</div>
                 <div className='nav-items'>
                     <div className='nav-item'>صفحه اصلی</div>
                     <div className='nav-item'>درباره ما</div>
