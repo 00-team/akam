001ce20132a8ad29f8bd22a1a4dd1ddf5f1fa749
sadra <sadrataghavi1383@gmail.com>
2022-08-31 11:59:42 +0430

    feat(home): add about

--- app/Home/index.tsx
+++ app/Home/index.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from 'react'
 
 import HeroSection from './HeroSection'
+import AboutUs from './aboutUs'
 
 import './style/home.scss'
 
@@ -9,6 +10,8 @@ const Home: FC = () => {
         <div className='home-container'>
             {/* sections */}
             <HeroSection />
+
+            <AboutUs />
         </div>
     )
 }
