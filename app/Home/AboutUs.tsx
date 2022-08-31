41b5df43655819d83883098068e63e77efd00110
sadra <sadrataghavi1383@gmail.com>
2022-08-31 13:31:45 +0430

    feat(about): jsx

--- app/Home/AboutUs.tsx
+++ app/Home/AboutUs.tsx
@@ -1,9 +1,26 @@
 import React from 'react'
 
+import aboutSvg from '../../static/svgs/about.svg'
+
 import './style/aboutus.scss'
 
 const AboutUs = () => {
-    return <section className='about-container'>aboutUs</section>
+    return (
+        <section className='about-container' id='about'>
+            <div className='about-wrapper'>
+                <div className='about-title title'>
+                    چرا <span>آکام؟</span>
+                </div>
+                <div className='about-content'>
+                    <div className='content-title title_hero'>درباره ما</div>
+                    <div className='about-contents'></div>
+                </div>
+            </div>
+            <div className='svg-container'>
+                <object data={aboutSvg} type=''></object>
+            </div>
+        </section>
+    )
 }
 
 export default AboutUs
