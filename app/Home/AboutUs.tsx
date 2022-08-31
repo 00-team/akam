2fd8d021a1f39e342edb1e63052c0e77138fb74a
sadra <sadrataghavi1383@gmail.com>
2022-08-31 17:22:51 +0430

    feat(about): design

--- app/Home/AboutUs.tsx
+++ app/Home/AboutUs.tsx
@@ -1,8 +1,8 @@
 import React, { FC } from 'react'
 
 import { IconType } from '@react-icons/all-files'
+import { FaGlobe } from '@react-icons/all-files/fa/FaGlobe'
 import { FaHandshake } from '@react-icons/all-files/fa/FaHandshake'
-import { SiAtom } from '@react-icons/all-files/si/SiAtom'
 
 import aboutSvg from '../../static/svgs/about.svg'
 
@@ -11,6 +11,9 @@ import './style/aboutus.scss'
 const AboutUs = () => {
     return (
         <section className='about-container' id='about'>
+            <div className='svg-container'>
+                <object data={aboutSvg} type=''></object>
+            </div>
             <div className='about-wrapper'>
                 <div className='about-title title'>
                     <span className='span-wrapper'>
@@ -23,21 +26,23 @@ const AboutUs = () => {
                         <div className='contents-wrapper'>
                             <AboutContent
                                 Svg={FaHandshake}
+                                className='trust'
                                 title='اعتماد'
                                 description='عملیات ها به طور موثر اجرا می شود و این شرکت یک پیشرو در صنعت است.  آکام با عرضه های اصیل و متمایز خود به رشد و توسعه خود ادامه می دهد... '
                             />
-                            <AboutContent
-                                Svg={SiAtom}
-                                title='نو آوری'
-                                description='اکام  با  پذیرفتن راه‌های جدید آینده، به نوآوری و برتری خود از طریق محصولات، خدمات و رویکرد مشتری‌محور معتبر و بی‌رقیب خود ادامه می دهد و برای مشتریان  کالایی با کیفیت بالا و بی رقیب ارائه می دهد 
-                                '
-                            />
-                        </div>
-                        <div className='contents-wrapper'>
                             <AboutContent
                                 Svg={FaHandshake}
                                 title='اعتماد'
                                 description='عملیات ها به طور موثر اجرا می شود و این شرکت یک پیشرو در صنعت است.  آکام با عرضه های اصیل و متمایز خود به رشد و توسعه خود ادامه می دهد... '
+                            />
+                        </div>
+                        <div className='contents-wrapper'>
+                            <AboutContent
+                                Svg={FaGlobe}
+                                className='innovation'
+                                title='نواوری'
+                                description='اکام  با  پذیرفتن راه‌های جدید آینده، به نوآوری و برتری خود از طریق محصولات، خدمات و رویکرد مشتری‌محور معتبر و بی‌رقیب خود ادامه می دهد و برای مشتریان  کالایی با کیفیت بالا و بی رقیب ارائه می دهد 
+                            '
                             />
                             <AboutContent
                                 Svg={FaHandshake}
@@ -48,23 +53,26 @@ const AboutUs = () => {
                     </div>
                 </div>
             </div>
-            <div className='svg-container'>
-                <object data={aboutSvg} type=''></object>
-            </div>
         </section>
     )
 }
 
 interface AboutContentProps {
     Svg: IconType
+    className?: string
     title: string
     description: string
 }
 
-const AboutContent: FC<AboutContentProps> = ({ Svg, title, description }) => {
+const AboutContent: FC<AboutContentProps> = ({
+    Svg,
+    title,
+    description,
+    className,
+}) => {
     return (
         <div className='about-content'>
-            <div className='about-svg'>
+            <div className={`about-svg ${className && className}`}>
                 <Svg size={30} />
             </div>
             <div className='about-title title_small'>{title}</div>
