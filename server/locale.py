a23bd08a751d3fb7fe58e3344617ab997d83ecf7
007 <dr007cc@gmail.com>
2022-08-31 13:49:39 +0430

    fix: validate the locale by schema

--- server/locale.py
+++ server/locale.py
@@ -1,6 +1,8 @@
 import json
 from pathlib import Path
 
+from jsonschema import validate
+
 from .shared import BASE_DIR
 
 
@@ -8,34 +10,32 @@ LOCALE_DIR = BASE_DIR / 'locale'
 DEFAULT_LOCALE = 'en'
 
 
+def get_schema():
+    with open(LOCALE_DIR / 'schema.json') as f:
+        return json.load(f)
+
+
 def load_locales(paths: list[Path]):
     locales = {}
     locales_list = []
+    schema = get_schema()
 
     for path in paths:
         with open(path) as f:
             data = json.load(f)
 
+        validate(data, schema)
+
         locale_id = path.name[:-5]
-        label = data['label']
-        content = data['content']
-        direction = data.get('direction', 'ltr')
 
-        if not direction in ['rtl', 'ltr']:
-            raise ValueError((
-                f'invalid direction: {direction} in {path.name}.\n            '
-                f'possible values are "rtl" & "ltr"'
-            ))
-
-        base = {
+        locales_list.append({
             'id': locale_id,
-            'label': label,
-        }
+            'label': data['label'],
+        })
 
-        locales_list.append(base)
         locales[locale_id] = {
-            **base,
-            'content': content
+            'id': locale_id,
+            **data,
         }
 
     if not DEFAULT_LOCALE in locales:
@@ -45,8 +45,8 @@ def load_locales(paths: list[Path]):
 
 
 def get_locales():
-    locales = LOCALE_DIR.glob('*.json')
-    locales = filter(lambda p: p.is_file(), locales)
+    files = LOCALE_DIR.glob('*.json')
+    locales = filter(lambda p: p.is_file() and p.name != 'schema.json', files)
     return load_locales(list(locales))
 
 
