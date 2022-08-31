07e7edc350d1d31ed8778ab5c46f3bd94ec25942
sadra <sadrataghavi1383@gmail.com>
2022-08-31 16:25:24 +0430

    feat(about)

--- app/Home/AboutUs.tsx
+++ app/Home/AboutUs.tsx
@@ -1,4 +1,8 @@
-import React from 'react'
+import React, { FC } from 'react'
+
+import { IconType } from '@react-icons/all-files'
+import { FaHandshake } from '@react-icons/all-files/fa/FaHandshake'
+import { SiAtom } from '@react-icons/all-files/si/SiAtom'
 
 import aboutSvg from '../../static/svgs/about.svg'
 
@@ -17,12 +21,29 @@ const AboutUs = () => {
                     <div className='content-title section_title'>درباره ما</div>
                     <div className='about-contents'>
                         <div className='contents-wrapper'>
-                            <AboutContent />
-                            <AboutContent />
+                            <AboutContent
+                                Svg={FaHandshake}
+                                title='اعتماد'
+                                description='عملیات ها به طور موثر اجرا می شود و این شرکت یک پیشرو در صنعت است.  آکام با عرضه های اصیل و متمایز خود به رشد و توسعه خود ادامه می دهد... '
+                            />
+                            <AboutContent
+                                Svg={SiAtom}
+                                title='نو آوری'
+                                description='اکام  با  پذیرفتن راه‌های جدید آینده، به نوآوری و برتری خود از طریق محصولات، خدمات و رویکرد مشتری‌محور معتبر و بی‌رقیب خود ادامه می دهد و برای مشتریان  کالایی با کیفیت بالا و بی رقیب ارائه می دهد 
+                                '
+                            />
                         </div>
                         <div className='contents-wrapper'>
-                            <AboutContent />
-                            <AboutContent />
+                            <AboutContent
+                                Svg={FaHandshake}
+                                title='اعتماد'
+                                description='عملیات ها به طور موثر اجرا می شود و این شرکت یک پیشرو در صنعت است.  آکام با عرضه های اصیل و متمایز خود به رشد و توسعه خود ادامه می دهد... '
+                            />
+                            <AboutContent
+                                Svg={FaHandshake}
+                                title='اعتماد'
+                                description='عملیات ها به طور موثر اجرا می شود و این شرکت یک پیشرو در صنعت است.  آکام با عرضه های اصیل و متمایز خود به رشد و توسعه خود ادامه می دهد... '
+                            />
                         </div>
                     </div>
                 </div>
@@ -34,8 +55,25 @@ const AboutUs = () => {
     )
 }
 
-const AboutContent = () => {
-    return <div className='about-content'></div>
+interface AboutContentProps {
+    Svg: IconType
+    title: string
+    description: string
+}
+
+const AboutContent: FC<AboutContentProps> = ({ Svg, title, description }) => {
+    return (
+        <div className='about-content'>
+            <div className='about-svg'>
+                <Svg size={30} />
+            </div>
+            <div className='about-title title_small'>{title}</div>
+            <div className='about-description description'>{description}</div>
+            <div className='see-more'>
+                <button>ادامه مطلب</button>
+            </div>
+        </div>
+    )
 }
 
 export default AboutUs
