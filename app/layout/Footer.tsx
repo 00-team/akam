7c3cc2879164a7b46fbbdb8e5da3390624557137
sadra <sadrataghavi1383@gmail.com>
2022-09-01 23:21:55 +0430

    fix(footer)

--- app/layout/Footer.tsx
+++ app/layout/Footer.tsx
@@ -3,7 +3,12 @@ import React from 'react'
 import './style/footer.scss'
 
 const Footer = () => {
-    return <div>Footer</div>
+    return (
+        <footer className='footer-container'>
+            <div className='header-section'></div>
+            <div className='content-section'></div>
+        </footer>
+    )
 }
 
 export default Footer
