6346d4739c951b406b2370ed476ef6266c549797
sadra <sadrataghavi1383@gmail.com>
2022-09-01 16:31:39 +0430

    feat(services):; init

---
+++ app/Home/Services.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import './style/services.scss'
+
+const Services = () => {
+    return <section className='services-container'>Services</section>
+}
+
+export default Services
