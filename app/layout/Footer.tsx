3e1900670b71d4f8ebcc9a37a5e0beb32f8f4ac8
sadra <sadrataghavi1383@gmail.com>
2022-09-02 18:32:57 +0430

    feat(footer): content

--- app/layout/Footer.tsx
+++ app/layout/Footer.tsx
@@ -9,48 +9,124 @@ import './style/footer.scss'
 const Footer = () => {
     return (
         <footer className='footer-container'>
-            <div className='header-section-wrapper title_small'>
-                <div className='header-section-container'>
-                    <div className='header-section header-call'>
-                        <div className='header-section-elems'>
-                            <div className='icon header-icon call'>
-                                {' '}
-                                <IoCall size={24} />
-                            </div>
-                            <div className='header-holder'>09120974956</div>
-                            <div className='header-svg'></div>
+            <FooterMainSection />
+            <ContentSection />
+        </footer>
+    )
+}
+
+const FooterMainSection = () => {
+    return (
+        <div className='header-section-wrapper title_small'>
+            <div className='header-section-container'>
+                <div className='header-section header-call'>
+                    <div className='header-section-elems'>
+                        <div className='icon header-icon call'>
+                            {' '}
+                            <IoCall size={24} />
                         </div>
-                    </div>
-                </div>
-                <div className='header-section-container'>
-                    <div className='header-section header-gmail'>
-                        <div className='header-section-elems'>
-                            <div className='icon header-icon gmail'>
-                                <MdEmail size={24} />
-                            </div>
-                            <div className='header-holder '>
-                                sadrataghavi1383@gmail.com
-                            </div>
-                            <div className='header-svg'></div>
-                        </div>
-                    </div>
-                </div>
-                <div className='header-section-container'>
-                    <div className='header-section header-location'>
-                        <div className='header-section-elems'>
-                            <div className='icon header-icon location'>
-                                <HiLocationMarker size={24} />
-                            </div>
-                            <div className='header-holder '>
-                                تهران، امیر اباد، خیابان عبداللهی، پلاک 20
-                            </div>
-                            <div className='header-svg'></div>
-                        </div>
+                        <div className='header-holder'>09120974956</div>
+                        <div className='header-svg'></div>
                     </div>
                 </div>
             </div>
-            <div className='content-section'></div>
-        </footer>
+            <div className='header-section-container'>
+                <div className='header-section header-gmail'>
+                    <div className='header-section-elems'>
+                        <div className='icon header-icon gmail'>
+                            <MdEmail size={24} />
+                        </div>
+                        <div className='header-holder '>
+                            sadrataghavi1383@gmail.com
+                        </div>
+                        <div className='header-svg'></div>
+                    </div>
+                </div>
+            </div>
+            <div className='header-section-container'>
+                <div className='header-section header-location'>
+                    <div className='header-section-elems'>
+                        <div className='icon header-icon location'>
+                            <HiLocationMarker size={24} />
+                        </div>
+                        <div className='header-holder '>
+                            تهران، امیر اباد، خیابان عبداللهی، پلاک 20
+                        </div>
+                        <div className='header-svg'></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+const ContentSection = () => {
+    return (
+        <div className='content-section'>
+            <div className='content-wrapper'>
+                <div className='content'>
+                    <div className='content-header title_small'>
+                        <span className='title-wrapper'>درباره ما</span>
+                    </div>
+                    <a className='content-link-wrapper' href=''>
+                        <div className='content-link title_smaller'>
+                            لورم ایپسون متن
+                        </div>
+                    </a>
+                    <a className='content-link-wrapper' href=''>
+                        <div className='content-link title_smaller'>
+                            لورم ایپسون متن
+                        </div>
+                    </a>
+                    <a className='content-link-wrapper' href=''>
+                        <div className='content-link title_smaller'>
+                            لورم ایپسون متن
+                        </div>
+                    </a>
+                </div>
+                <div className='content'>
+                    <div className='content-header title_small'>
+                        <span className='title-wrapper'>خدمات</span>
+                    </div>
+                    <a className='content-link-wrapper' href=''>
+                        <div className='content-link title_smaller'>
+                            لورم ایپسون متن
+                        </div>
+                    </a>
+                    <a className='content-link-wrapper' href=''>
+                        <div className='content-link title_smaller'>
+                            لورم ایپسون متن
+                        </div>
+                    </a>
+                    <a className='content-link-wrapper' href=''>
+                        <div className='content-link title_smaller'>
+                            لورم ایپسون متن
+                        </div>
+                    </a>
+                </div>
+                <div className='content'>
+                    <div className='content-header title_small'>
+                        <span className='title-wrapper'>ارتباط با ما</span>
+                    </div>
+                    <a className='content-link-wrapper' href=''>
+                        <div className='content-link title_smaller'>
+                            لورم ایپسون متن
+                        </div>
+                    </a>
+                    <a className='content-link-wrapper' href=''>
+                        <div className='content-link title_smaller'>
+                            لورم ایپسون متن
+                        </div>
+                    </a>
+                    <a className='content-link-wrapper' href=''>
+                        <div className='content-link title_smaller'>
+                            لورم ایپسون متن
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div className='logo-wrapper title_hero'>LOGO</div>
+        </div>
     )
 }
 
