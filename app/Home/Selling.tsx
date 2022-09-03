3be7ee379de243d78e1c35c23df3a054849ca656
sadra <sadrataghavi1383@gmail.com>
2022-09-03 14:57:57 +0430

    feat(selling): init

---
+++ app/Home/Selling.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import './style/selling.scss'
+
+const Selling = () => {
+    return (
+        <section className='sell-container'>
+            <div className='svg-container'></div>
+            <div className='sell-wrapper'></div>
+        </section>
+    )
+}
+
+export default Selling
