1085ffd69d8c1898615e82c6ba1f63f51a8dd6c3
sadra <sadrataghavi1383@gmail.com>
2022-09-04 15:50:28 +0430

    fix(isintersecting):

---
+++ app/components/IsIntersecting.tsx
@@ -0,0 +1,46 @@
+import React, { FC, HTMLAttributes, ReactNode, useEffect, useRef } from 'react'
+
+// type attrs = Omit<HTMLAttributes<HTMLDivElement>, 'children'>
+interface IsIntersectingProps extends HTMLAttributes<HTMLDivElement> {
+    children: ReactNode
+    options?: IntersectionObserverInit
+
+    Tag: React.ElementType
+    Intersected: boolean
+    setState?: (state: boolean) => void
+}
+
+const IsIntersecting: FC<IsIntersectingProps> = ({
+    children,
+    options,
+    Intersected,
+    setState,
+    Tag,
+    ...attrs
+}) => {
+    const container = useRef<HTMLDivElement>(null)
+
+    useEffect(() => {
+        if (container.current && !Intersected) {
+            var observer = new IntersectionObserver(([entry]) => {
+                if (entry && entry.isIntersecting) {
+                    setState && setState(true)
+                    observer.disconnect()
+                }
+            }, options)
+
+            observer.observe(container.current)
+        }
+        return () => {
+            if (observer) observer.disconnect()
+        }
+    }, [container])
+
+    return (
+        <Tag ref={container} {...attrs}>
+            {children}
+        </Tag>
+    )
+}
+
+export default IsIntersecting
