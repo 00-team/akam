f23197d6fcc1517c6a1badc8a083e62a314d5ea0
sadra <sadrataghavi1383@gmail.com>
2022-09-05 16:21:50 +0430

    fix(contact): design-jsx update

--- app/Home/ContactUs.tsx
+++ app/Home/ContactUs.tsx
@@ -1,6 +1,12 @@
-import React, { useState } from 'react'
+import React, { FC, useState } from 'react'
 
 import { C } from '@00-team/utils'
+
+import { IconType } from '@react-icons/all-files'
+import { FaFax } from '@react-icons/all-files/fa/FaFax'
+import { MdLocationCity } from '@react-icons/all-files/md/MdLocationCity'
+import { MdPhoneInTalk } from '@react-icons/all-files/md/MdPhoneInTalk'
+import { SiGmail } from '@react-icons/all-files/si/SiGmail'
 
 import IsIntersecting from 'components/IsIntersecting'
 
@@ -29,7 +35,35 @@ const ContactUs = () => {
                         <div className='contact-content-title title_hero'>
                             راه های ارتباطی با ما
                         </div>
-                        <div className='contact-content'></div>
+                        <div className='contact-content'>
+                            <div className='contact-columns'>
+                                <ContactColumn
+                                    holder='sadrataghavi1383@gmail.com'
+                                    className='gmail'
+                                    Icon={SiGmail}
+                                />
+                                <ContactColumn
+                                    className='phone'
+                                    holder='09120974956'
+                                    Icon={MdPhoneInTalk}
+                                />
+                            </div>
+                            <div className='contact-columns'>
+                                <ContactColumn
+                                    className='location'
+                                    holder='تهران، امیر اباد، خیابان عبداللهی، پلاک 20'
+                                    Icon={MdLocationCity}
+                                />
+                                <ContactColumn
+                                    className='fax'
+                                    holder='13132546'
+                                    Icon={FaFax}
+                                />
+                            </div>
+                        </div>
+                    </div>
+                    <div className='contact-see-more'>
+                        <button>see more</button>
                     </div>
                 </div>
                 <div className='svg-container'>
@@ -37,6 +71,25 @@ const ContactUs = () => {
                 </div>
             </IsIntersecting>
         </section>
+    )
+}
+
+interface ContactColumnProps {
+    Icon: IconType
+    holder: string
+    className?: string
+}
+
+const ContactColumn: FC<ContactColumnProps> = ({ holder, Icon, className }) => {
+    return (
+        <div
+            className={`contact-column title_smaller ${className && className}`}
+        >
+            <div className='column-icon'>
+                <Icon size={25} />
+            </div>
+            <div className='column-holder'>{holder}</div>
+        </div>
     )
 }
 
