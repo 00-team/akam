9126de87a5bdffc2cda13fb0b1c2cf2704d17ee4
sadra <sadrataghavi1383@gmail.com>
2022-09-11 20:08:30 +0430

    feat(contact): contact page init

---
+++ app/ContactUs/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import './style/contactus.scss'
+
+const ContactUsPage = () => {
+    return (
+        <section className='contact-container'>
+            <div className='contact-header'></div>
+            <div className='contact-wrapper'>
+                <div className='contact-socials'></div>
+                <div className='contact-loc_form'>
+                    <div className='contact-form'></div>
+                    <div className='contact-location'></div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default ContactUsPage
