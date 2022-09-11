fd90b3cb5604211fe081cc97e185905bf42413e0
sadra <sadrataghavi1383@gmail.com>
2022-09-11 18:45:09 +0430

    feat(loading): jsx update

--- app/components/Loading.tsx
+++ app/components/Loading.tsx
@@ -6,11 +6,16 @@ import './style/loading.scss'
 
 const Loading = () => {
     return (
-        <div className='site-loading' attr-image={loadingImg}>
-            <div className='loading-title title_hero'>
-                به سایت <span className='colored-word'>آکام</span> خوش امدید
+        <div
+            className='site-loading'
+            style={{ backgroundImage: `url(${loadingImg})` }}
+        >
+            <div className='titles-wrapper'>
+                <div className='loading-title title_hero'>
+                    به سایت <span className='colored-word'>آکام</span> خوش امدید
+                </div>
+                <div className='title'>سایت در حال برگزاری است...</div>
             </div>
-            <div className='title'>سایت در حال برگزاری است...</div>
         </div>
     )
 }
