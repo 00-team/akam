798152e6b295a09341cabf30268fd45338a786cc
sadra <sadrataghavi1383@gmail.com>
2022-09-11 21:03:01 +0430

    fix(contact): socials update

--- app/ContactUs/index.tsx
+++ app/ContactUs/index.tsx
@@ -1,13 +1,72 @@
 import React from 'react'
 
+import { FiPhoneCall } from '@react-icons/all-files/fi/FiPhoneCall'
+import { SiGmail } from '@react-icons/all-files/si/SiGmail'
+import { SiTelegram } from '@react-icons/all-files/si/SiTelegram'
+import { SiWhatsapp } from '@react-icons/all-files/si/SiWhatsapp'
+
+import HeaderImg from '../../static/imgs/ContactHeader.jpg'
+
 import './style/contactus.scss'
 
 const ContactUsPage = () => {
+    const IsMobile = innerWidth <= 1024
     return (
-        <section className='contact-container'>
-            <div className='contact-header'></div>
+        <section className='contact-page-container'>
+            <div
+                className='contact-header'
+                style={{
+                    backgroundImage: `linear-gradient(
+                    rgba(0, 0, 0, 0.5),
+                    rgba(0, 0, 0, 0.5)
+                  ),
+                url(${HeaderImg})`,
+                }}
+            >
+                <div className='header-text title_hero'>
+                    تماس با <span className='colored-word'>آکام</span>
+                </div>
+            </div>
             <div className='contact-wrapper'>
-                <div className='contact-socials'></div>
+                <div className='contact-socials-wrapper'>
+                    <div className='contact-socials-title title_hero'>
+                        راه های ارتباطی ما
+                    </div>
+                    <div
+                        className={`contact-socials title_smaller ${
+                            IsMobile ? 'vertical' : 'horizental'
+                        } `}
+                    >
+                        <a
+                            className='social-icon call'
+                            style={{ animationDelay: `.5s` }}
+                        >
+                            <FiPhoneCall size={35} className='icon-svg' />
+                            <div className='tooltip'>09120974956</div>
+                        </a>
+                        <a
+                            className='social-icon instagram'
+                            style={{ animationDelay: `1s` }}
+                        >
+                            <SiGmail size={35} className='icon-svg' />
+                            <div className='tooltip'>@Sadra_Tghvi</div>
+                        </a>
+                        <a
+                            className='social-icon facebook'
+                            style={{ animationDelay: `.7s` }}
+                        >
+                            <SiWhatsapp size={35} className='icon-svg' />
+                            <div className='tooltip'>09120974956</div>
+                        </a>
+                        <a
+                            className='social-icon tel'
+                            style={{ animationDelay: `1.25s` }}
+                        >
+                            <SiTelegram size={35} className='icon-svg' />
+                            <div className='tooltip'>@sadrataghavi3</div>
+                        </a>
+                    </div>
+                </div>
                 <div className='contact-loc_form'>
                     <div className='contact-form'></div>
                     <div className='contact-location'></div>
