8e5bef6c843bb8d6032f2940d47681bb0f7f1bfc
sadra <sadrataghavi1383@gmail.com>
2022-09-11 18:31:57 +0430

    feat(loading): improvments

--- app/components/Loading.tsx
+++ app/components/Loading.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
 
+import loadingImg from '../../static/imgs/loading.jpg'
+
 import './style/loading.scss'
 
 const Loading = () => {
-    return <div className='site-loading'>Loading</div>
+    return (
+        <div className='site-loading' attr-image={loadingImg}>
+            <div className='loading-title title_hero'>
+                به سایت <span className='colored-word'>آکام</span> خوش امدید
+            </div>
+            <div className='title'>سایت در حال برگزاری است...</div>
+        </div>
+    )
 }
 
 export default Loading
