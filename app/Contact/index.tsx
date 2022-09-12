c61c6fb20fe7478837b82ee287826ae369d52d01
sadra <sadrataghavi1383@gmail.com>
2022-09-12 14:40:24 +0430

    feat(contact): modulared

--- app/Contact/index.tsx
+++ app/Contact/index.tsx
@@ -1,6 +1,8 @@
 import React, { FC } from 'react'
 
+import ContactForm from './ContactForm'
 import { ContactHeader } from './ContactHeader'
+import ContactLocation from './ContactLocation'
 import { ContactSocials } from './ContactSocials'
 
 import './style/contactus.scss'
@@ -12,47 +14,8 @@ const Contact: FC = () => {
             <div className='contact-wrapper'>
                 <ContactSocials />
                 <div className='contact-loc_form'>
-                    <form className='contact-form '>
-                        <fieldset>
-                            <legend className='title'>
-                                پیام مستقیم به{' '}
-                                <span className='colored-word'>آکام؟</span>
-                            </legend>
-                            <div className='title-inps'>
-                                <input
-                                    type='text'
-                                    className='name title_smaller'
-                                    name='name'
-                                    placeholder='نام'
-                                />
-                                <input
-                                    type='text'
-                                    className='lastname title_smaller'
-                                    name='lastname'
-                                    placeholder='نام خانوادگی'
-                                />
-                            </div>
-                            <input
-                                type='text'
-                                className='gmail title_smaller'
-                                placeholder='example@gmail.com'
-                                name='gmail'
-                            />
-                            <textarea
-                                placeholder='متن پیام...'
-                                className='title_smaller'
-                                name='message'
-                                cols={30}
-                                rows={10}
-                            ></textarea>
-                            <div className='cta-wrapper'>
-                                <button className='cta title_smaller'>
-                                    ارسال
-                                </button>
-                            </div>
-                        </fieldset>
-                    </form>
-                    <div className='contact-location'></div>
+                    <ContactForm />
+                    <ContactLocation />
                 </div>
             </div>
         </section>
