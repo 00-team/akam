6d07910b8885486201442682ce8525e0cd01c6bf
sadra <sadrataghavi1383@gmail.com>
2022-09-12 12:27:59 +0430

    fix(contact)

--- app/ContactUs/index.tsx
+++ app/ContactUs/index.tsx
@@ -12,7 +12,39 @@ const ContactUsPage = () => {
             <div className='contact-wrapper'>
                 <ContactSocials />
                 <div className='contact-loc_form'>
-                    <div className='contact-form'></div>
+                    <form className='contact-form '>
+                        <fieldset>
+                            <legend className='title'>
+                                پیام مستقیم به{' '}
+                                <span className='colored-word'>آکام؟</span>
+                            </legend>
+                            <div className='title-inps'>
+                                <input
+                                    type='text'
+                                    className='name title_small'
+                                    name='name'
+                                    placeholder='نام'
+                                />
+                                <input
+                                    type='text'
+                                    className='lastname title_small'
+                                    name='lastname'
+                                    placeholder='نام خانوادگی'
+                                />
+                            </div>
+                            <input
+                                type='text'
+                                className='gmail title_small'
+                                name='gmail'
+                            />
+                            <textarea
+                                className='title_small'
+                                name='message'
+                                cols={30}
+                                rows={10}
+                            ></textarea>
+                        </fieldset>
+                    </form>
                     <div className='contact-location'></div>
                 </div>
             </div>
