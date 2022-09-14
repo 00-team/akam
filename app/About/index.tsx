7641e0f24c168f52f8ca3cb23f27ac790a411588
sadra <sadrataghavi1383@gmail.com>
2022-09-14 13:55:28 +0430

    fix(about)

--- app/About/index.tsx
+++ app/About/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 
+import AboutCard from './AboutCard'
 import AboutHeader from './AboutHeader'
 
 import './style/about.scss'
@@ -9,14 +10,17 @@ const About = () => {
         <div className='about-page-container'>
             <AboutHeader />
             <section className='about-wrapper'>
-                <section className='about-creators'>
+                <div className='about-creators'>
                     <header className='creators-title title_hero'>
                         <div className='header-wrapper'>
                             دستندرکاران{' '}
                             <span className='colored-word'>آکام</span>
                         </div>
                     </header>
-                </section>
+                    <div className='creators-wrapper'>
+                        <AboutCard />
+                    </div>
+                </div>
             </section>
         </div>
     )
