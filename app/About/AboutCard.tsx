80f879010a1bce823154895e737447bd0db03c10
sadra <sadrataghavi1383@gmail.com>
2022-09-14 13:55:15 +0430

    feat(about): card init

---
+++ app/About/AboutCard.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const AboutCard = () => {
+    return (
+        <div className='about-card-container'>
+            <div className='card-img'></div>
+            <div className='card-title title_small'></div>
+            <div className='card-img description'></div>
+        </div>
+    )
+}
+
+export default AboutCard
