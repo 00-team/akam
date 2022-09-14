2b1da4e20571aae8fc19eeb8b454c60f0a883eb0
sadra <sadrataghavi1383@gmail.com>
2022-09-14 13:39:47 +0430

    fix(about)

--- app/About/index.tsx
+++ app/About/index.tsx
@@ -1,7 +1,25 @@
 import React from 'react'
 
+import AboutHeader from './AboutHeader'
+
+import './style/about.scss'
+
 const About = () => {
-    return <div>About</div>
+    return (
+        <div className='about-page-container'>
+            <AboutHeader />
+            <section className='about-wrapper'>
+                <section className='about-creators'>
+                    <header className='creators-title title_hero'>
+                        <div className='header-wrapper'>
+                            دستندرکاران{' '}
+                            <span className='colored-word'>آکام</span>
+                        </div>
+                    </header>
+                </section>
+            </section>
+        </div>
+    )
 }
 
 export default About
