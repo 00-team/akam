19edc3a534fcfa31a398df5772f6e2e30605aeb4
sadra <sadrataghavi1383@gmail.com>
2022-09-20 13:48:09 +0430

    fix(selling)

--- app/selling/index.tsx
+++ app/selling/index.tsx
@@ -5,7 +5,19 @@ import './style/selling.scss'
 const Selling = () => {
     return (
         <main className='selling-container'>
-            <header className='selling-header'></header>
+            <header
+                className='selling-header'
+                style={{
+                    backgroundImage: `
+    linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
+    url(/static/image/contact-header.jpg)`,
+                }}
+            >
+                <div className='header-text title_hero'>
+                    استراتژی های فروش <span className='colored-word'>آکام</span>
+                </div>
+            </header>
+            <section className='selling-wrapper'></section>
         </main>
     )
 }
