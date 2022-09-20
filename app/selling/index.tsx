a565db1250b955868c397ad9a2bbbbf1f1895538
sadra <sadrataghavi1383@gmail.com>
2022-09-20 13:00:17 +0430

    feat(selling): style improve

--- app/selling/index.tsx
+++ app/selling/index.tsx
@@ -3,7 +3,11 @@ import React from 'react'
 import './style/selling.scss'
 
 const Selling = () => {
-    return <main className='selling-container'>Selling</main>
+    return (
+        <main className='selling-container'>
+            <header className='selling-header'></header>
+        </main>
+    )
 }
 
 export default Selling
