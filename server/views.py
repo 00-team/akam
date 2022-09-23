f95d14cfd79b566305125fb13877a70fe6cf45b9
007 <dr007cc@gmail.com>
2022-09-24 03:11:24 +0330

    feat: locale for errors

--- server/views.py
+++ server/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, request
+from flask import render_template, request, session
 from werkzeug.exceptions import HTTPException
 
 from .locale import DEFAULT_LOCALE, LOCALES, LOCALES_LIST
@@ -21,21 +21,32 @@ def locales_list():
 
 
 def get_locale():
-    locale = request.args.get('locale', DEFAULT_LOCALE)
+    # basicly: locale from request OR locale from session OR DEFAULT_LOCALE
+    session_locale = session.get('locale', DEFAULT_LOCALE)
+    locale = request.args.get('locale', session_locale)
     data = LOCALES.get(locale)
 
+    # if requested locale is not defined we use the default one
     if data is None:
-        data = LOCALES.get(DEFAULT_LOCALE)
+        data = LOCALES[DEFAULT_LOCALE]
+    else:
+        session['locale'] = locale
 
     return data
 
 
 def errors(error: HTTPException):
+    locale = session.get('locale', DEFAULT_LOCALE)
+    content = LOCALES[locale]['errors']
+    code = str(error.code)
 
-    context = {
-        'code': error.code,
-        'title': error.name,
-        'description': error.description,
-    }
+    if code in content:
+        context = {'code': code, **content[code]}
+    else:
+        context = {
+            'code': code,
+            'title': error.name,
+            'description': error.description,
+        }
 
     return render_template('error.html', error=context), error.code
